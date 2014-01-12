338ae5f813b2a8ca89921f956f02748ea402eae9
Marco Munizaga <marco.munizaga@ufl.edu>
2014-01-12 01:39:22 -0500

    added canvas drawer

--- project.clj
+++ project.clj
@@ -3,4 +3,17 @@
   :url "https://github.com/mediocregopher/diamond-square"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2138"]]
+
+  :source-paths ["src"]
+  :plugins [[lein-cljsbuild "1.0.1"]]
+
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "app.js"
+                :output-dir "out"
+                :optimizations :simple
+                :source-map "app.js.map"}}]})
---
+++ src/diamond_square/canvas_renderer.cljs
@@ -0,0 +1,36 @@
+(ns diamond-square.canvas-renderer
+  (:require [diamond-square.core :as core]))
+
+(.log js/console "hello world")
+(set! *print-fn* #(.log js/console %))
+
+#_(core/print-m
+  (core/tile-terrain
+    (core/terrain 5)))
+
+
+(def canvas (.getElementById js/document "terrain"))
+(def ctx (.getContext canvas "2d"))
+
+
+(def colors
+  ["#1437AD" "#04859D" "#007D1C" "#007D1C" "#24913C"
+   "#00C12B" "#38E05D" "#A3A3A4" "#757575" "#FFFFFF"])
+
+(defn img-terrain
+  [m]
+  (reduce
+    (fn [rown row]
+      (reduce
+        (fn [coln tile]
+          (set! (.-fillStyle ctx) (colors tile))
+          (.fillRect ctx coln rown 1 1)
+          #_(.setRGB img coln rown (colors tile))
+          (inc coln))
+        0 row)
+      (inc rown))
+    0 (core/normalize-terrain m (dec (count colors)))))
+
+(img-terrain (core/terrain 9))
+
+(.log js/console "Printing on canvas")
